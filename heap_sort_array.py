b4ad8a0dd892ed9dd0140e5a6da39e9b00807115
Z_BM <58821444+Nayjer@users.noreply.github.com>
2021-12-10 13:48:10 +0100

    Update heap_sort_array.py

--- heap_sort_array.py
+++ heap_sort_array.py
@@ -1,11 +1,11 @@
 import math
 
-array = ["x", 2, 1, 0, 22, 6]
+array = ["x", 110, 514, -239, -428, -342, 644, -762, -985, 85, 619, -712, 985, 579, -57, 254, 151, 500, 692, 171, 874, -312, 222, 159, 880, 90, 916, 613, 747, 539, 420]
 
 
 def heapify_array(arr):
     height = int(math.log2(len(arr) - 1) // 1)
-    for k in range(height):
+    for k in range(height):  # (log n)^2 * n comparisons
         for level in range(height, k, -1):
             for i in range(2 ** level, 2 ** (level + 1), 2):
                 if i + 1 > len(arr):
@@ -21,12 +21,9 @@ def heapify_array(arr):
     return arr
 
 
-sorted_array = []
-
-
-def heap_sort(arr):
+def heap_sort(arr, sorted_array):
     if len(arr) == 1:
-        return arr
+        return sorted_array
     arr[1], arr[len(arr) - 1] = arr[len(arr) - 1], arr[1]
     sorted_array.append(arr[len(arr) - 1])
     arr.pop()
@@ -35,9 +32,9 @@ def heap_sort(arr):
         if arr[i] < arr[2 * i]:
             arr[i], arr[2 * i] = arr[2 * i], arr[i]
         else:
-            return heap_sort(arr)
+            return heap_sort(arr, sorted_array)
     else:
-        while 2 * i < len(array) - 1:
+        while 2 * i < len(array) - 1:  # log n * n comparisons
             if arr[i] < arr[2 * i] and arr[2 * i] > arr[2 * i + 1]:
                 arr[i], arr[2 * i] = arr[2 * i], arr[i]
                 i = 2 * i
@@ -45,11 +42,9 @@ def heap_sort(arr):
                 arr[i], arr[2 * i + 1] = arr[2 * i + 1], arr[i]
                 i = 2 * i + 1
             else:
-                return heap_sort(arr)
-    return heap_sort(arr)
+                return heap_sort(arr, sorted_array)
+    return heap_sort(arr, sorted_array)
 
 
 arr1 = heapify_array(array)
-print(arr1)
-heap_sort(arr1)
-print(sorted_array)
+print(heap_sort(arr1, []))
